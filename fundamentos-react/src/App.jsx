dbf8e58dd7e28ae14efb0401cdd90436af1b931a
Rendell Arruda <rendellarruda3@gmail.com>
2023-03-19 20:52:04 -0300

    contador basico

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -9,48 +9,59 @@ import CondiconalComIf from './components/basicos/CondiconalComIf';
 import Pai from './components/comunicacao/direta/Pai';
 import Super from './components/comunicacao/indireta/Super';
 import Input from './components/form/Input';
+import Contador from './components/contador/Contador';
+
 export default props => (
   <div className="App">
-    <Card titulo=" #01 - Exemplo do Primeiro Componente" color="#FA6900">
-      <Primeiro />
-    </Card>
+    <h1>Fundamento React</h1>
+    <div className="Cards">
+      <Card titulo=" #01 - Exemplo do Primeiro Componente" color="#FA6900">
+        <Primeiro />
+      </Card>
 
-    <Card titulo="#02 - Exemplo com parâmetros" color="#E94C6F">
-      <ComParametros titulo="opa" subtitulo="epa" />
-      <ComParametros titulo="Esse é o titulo" subtitulo="este é o subtitulo" />
-    </Card>
+      <Card titulo="#02 - Exemplo com parâmetros" color="#E94C6F">
+        <ComParametros titulo="opa" subtitulo="epa" />
+        <ComParametros
+          titulo="Esse é o titulo"
+          subtitulo="este é o subtitulo"
+        />
+      </Card>
 
-    <Card titulo="#03 - Exercício com filhos" color="#008BBA">
-      <ComFilhos>
-        <ul>
-          <li>Ana</li>
-          <li>Bia</li>
-          <li>Carlos</li>
-          <li>Daniel</li>
-        </ul>
-      </ComFilhos>
-    </Card>
+      <Card titulo="#03 - Exercício com filhos" color="#008BBA">
+        <ComFilhos>
+          <ul>
+            <li>Ana</li>
+            <li>Bia</li>
+            <li>Carlos</li>
+            <li>Daniel</li>
+          </ul>
+        </ComFilhos>
+      </Card>
 
-    <Card titulo="#4 - Exercicio de repetiçao" color="#D96459">
-      <Repeticao></Repeticao>
-    </Card>
+      <Card titulo="#4 - Exercicio de repetiçao" color="#D96459">
+        <Repeticao></Repeticao>
+      </Card>
 
-    <Card titulo="#5 - Exercicio de Condicional  v01" color="#FF85CB">
-      <Condicional numero={11}></Condicional>
-    </Card>
+      <Card titulo="#5 - Exercicio de Condicional  v01" color="#FF85CB">
+        <Condicional numero={11}></Condicional>
+      </Card>
 
-    <Card titulo="#6 - Exercicio de Condicional  v02" color="#92B064">
-      <CondiconalComIf numero={22}></CondiconalComIf>
-    </Card>
+      <Card titulo="#6 - Exercicio de Condicional  v02" color="#92B064">
+        <CondiconalComIf numero={22}></CondiconalComIf>
+      </Card>
 
-    <Card titulo="#7 - Comunicação direta" color="#920">
-      <Pai sobrenome="Freitas"></Pai>
-    </Card>
-    <Card titulo="#8 - Comunicação Indireta" color="#4d0099">
-      <Super></Super>
-    </Card>
-    <Card titulo="#9 - Input" color="#6497b0">
-      <Input></Input>
-    </Card>
+      <Card titulo="#7 - Comunicação direta" color="#920">
+        <Pai sobrenome="Freitas"></Pai>
+      </Card>
+      <Card titulo="#8 - Comunicação Indireta" color="#4d0099">
+        <Super></Super>
+      </Card>
+      <Card titulo="#9 - Input" color="#6497b0">
+        <Input></Input>
+      </Card>
+      <Card titulo="#10 - Contador" color="#1133b0">
+        <Contador passo={10} valor={100}></Contador>
+      </Card>
+    </div>
   </div>
 );
